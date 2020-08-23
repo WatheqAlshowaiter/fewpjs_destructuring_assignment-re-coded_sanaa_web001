f84acd680ed5b1037a3f0eccbaaddb95ec2785d7
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-23 16:18:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,15 +54,7 @@ const [red, orange, yellow, green, blue, indigo, violet] = [
 ];
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-const [r, o, y, g, b, i, v] = [
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "indigo",
-  "violet",
-];
+const [r, o, y, g, b, i, v] = colors;
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 //     1) Array: uses destructuring to declare Indigo using indg
