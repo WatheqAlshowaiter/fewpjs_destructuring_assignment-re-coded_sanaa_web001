103ef5cc11f6db5cb91aa1055b40db1f35b50986
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-23 16:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const [r, o, y, g, b, i, v] = [
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 //     1) Array: uses destructuring to declare Indigo using indg
-const [, , , , , indig] = colors
+const [, , , , , indg] = colors
 
 // Objects
 
